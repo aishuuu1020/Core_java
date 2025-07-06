fa018162d6a1259101c3d0af249a1a1a7f57fd39
aishuuu1020 <aishwaryasonawane191627@gmail.com>
2025-07-06 20:29:58 +0530

    first java code

---
+++ src/lec1.java
@@ -0,0 +1,34 @@
+public class lec1 {
+public static void main(String args[])
+{
+
+    int num1 = 7;
+    num1 *= 20;
+    num1++;
+    ++num1;
+    int result = ++num1;
+    int result = ++num1;  // first increment and then fetch the value
+    System.out.println(result);
+
+
+
+    int num2 = 40;
+    int sum = num1 % num2;
+    System.out.println(sum);
+  float marks = 6.55f;
+  int num1 =8;
+  byte by = 127;
+  short sh =555;
+  float f = 5.8f;
+  char c = 't';
+  boolean b = true;
+    System.out.println(marks);
+
+
+    //literals
+    int num1 = 10_00_00_00_00;
+    System.out.println(num1);
+}
+}
+
+
